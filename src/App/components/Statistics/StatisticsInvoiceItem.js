860a30461a9b955a14f3a4224fbda2734d9ccf6d
Ivan Ivanov <ivan.ivanov@tibbo.com>
2020-11-30 01:47:02 +0300

    Refactor

--- src/App/components/Statistics/StatisticsInvoiceItem.js
+++ src/App/components/Statistics/StatisticsInvoiceItem.js
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { Button } from "antd";
 import { useState } from "react";
+import { useSwipeable } from "react-swipeable";
 import { getDateString, notify } from "../../utils";
 import { connectToStore } from "../../store/ConnectHolder";
 
 const StatisticsInvoiceItem = (props) => {
   const { date, item } = props;
-  const [timer, setTimer] = useState(null);
-  const [showDeleteButton, setShowDeleteButton] = useState(false);
-  const [positionX, setPositionX] = useState({ initialX: 0, currentX: 0 });
+  const [deleteButtonWidth, setDeleteButtonWidth] = useState(0);
 
   const removeItem = () => {
     const conf = window.confirm(`Вы уверены, что хотите удалить запись?`);
@@ -22,11 +21,19 @@ const StatisticsInvoiceItem = (props) => {
     }
   };
 
-  const deleteButton = () => {
+  const deleteButton = (width) => {
+    const styles =
+      width > 0
+        ? { visibility: "visible", flex: width }
+        : { visibility: "hidden", padding: 0, flexBasis: 0 };
+
     return (
       <Button
         className={"delete-button"}
-        style={{ transition: "250ms ease-in-out" }}
+        style={{
+          transition: "250ms ease-in-out",
+          ...styles,
+        }}
         onClick={removeItem}
       >
         Удалить
@@ -34,41 +41,35 @@ const StatisticsInvoiceItem = (props) => {
     );
   };
 
-  const handleMove = (event) => {
-    const x =
-      event.type === "touchmove" ? event.touches[0].clientX : event.clientX;
-    setPositionX({ ...positionX, currentX: x });
-    if (positionX.initialX - positionX.currentX > 100) {
-      setShowDeleteButton(true);
-    }
-    if (positionX.currentX - positionX.initialX > 100) {
-      setShowDeleteButton(false);
-    }
+  const config = {
+    delta: 0, // min distance(px) before a swipe starts
+    preventDefaultTouchmoveEvent: false, // call e.preventDefault *See Details*
+    trackTouch: true, // track touch input
+    trackMouse: true, // track mouse input
+    rotationAngle: 0, // set a rotation angle
   };
 
-  const handlePress = (event) => {
-    setTimer(setTimeout(() => setShowDeleteButton(true), 1000));
-    const x =
-      event.type === "touchstart" ? event.touches[0].clientX : event.clientX;
-    setPositionX({ ...positionX, initialX: x });
-  };
-
-  const handleReleasePress = (event) => {
-    clearTimeout(timer);
-    setPositionX({ initialX: 0, currentX: 0 });
-  };
+  const handlers = useSwipeable({
+    onSwipedLeft: (eventData) => setDeleteButtonWidth(1),
+    onSwipedRight: (eventData) => setDeleteButtonWidth(0),
+    onSwiping: (eventData) => {
+      if (eventData.absX < 100 && eventData.dir === "Left") {
+        setDeleteButtonWidth(eventData.absX / 100);
+      }
+    },
+    ...config,
+  });
 
   return (
     <div
+      {...handlers}
       className={"statistics"}
-      style={{ flexDirection: "row", flexWrap: "wrap", marginBottom: 20 }}
-      onMouseDown={handlePress}
-      onMouseUp={handleReleasePress}
-      onTouchStart={handlePress}
-      onTouchEnd={handleReleasePress}
-      onTouchMove={handleMove}
-      onMouseMove={handleMove}
-      onBlur={() => setShowDeleteButton(false)}
+      style={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+        marginBottom: 20,
+        marginTop: 10,
+      }}
     >
       <div style={{ flex: 2 }}>{getDateString(date)}</div>
       <div style={{ flex: 3, flexDirection: "column" }}>
@@ -80,7 +81,7 @@ const StatisticsInvoiceItem = (props) => {
           </div>
         ))}
       </div>
-      {showDeleteButton && deleteButton()}
+      {deleteButton(deleteButtonWidth)}
     </div>
   );
 };
